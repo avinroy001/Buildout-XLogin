954ecd98aa806b4f9ae0088558122a8a6d1338fd
Avinash <avin.roy001@gmail.com>
2024-02-29 12:35:59 +0530

    new update

--- xlogin/src/App.jsx
+++ xlogin/src/App.jsx
@@ -3,33 +3,37 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  let u='user';
-  let p='password';
   const [user, setUser] = useState('');
   const [pass,setPass] = useState('');
   const [readOnly,setReadOnly] = useState(false);
   const [msg,setMsg]=useState(false);
   const check=(e)=>{
     e.preventDefault();
-    if(u===user.trim() && p===pass.trim()){
+    if(user==='user' && pass==='password'){
       setReadOnly(true);
     }else{
       setMsg(true);
     }
   }
   return (
-    <>
+    <div>
     <h1>Login Page</h1>
     {msg?<p>Invalid username or password</p>:''}
     {readOnly ? (<p>Welcome, {user}!</p>):(
-    <form className='form' onSubmit={(e)=>check(e)}>
-      <label>Username:<input type='text' onChange={(e)=>{setUser(e.target.value)}} placeholder='username' required/></label>
-      <label>Password:<input type='text' onChange={(e)=>{setPass(e.target.value)}} placeholder='password' required/></label>
+    <form  onSubmit={(e)=>check(e)}>
+      <div>
+      <label>Username:</label>
+      <input type='text' onChange={(e)=>{setUser(e.target.value)}} placeholder='username' required/>
+      </div>
+      <div>
+      <label>Password:</label>
+      <input type='text' onChange={(e)=>{setPass(e.target.value)}} placeholder='password' required/>
+      </div>
       <button type='submit'>Submit</button>
     </form>
     )
 }
-    </>
+    </div>
   )
 }
 
